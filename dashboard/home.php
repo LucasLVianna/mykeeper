2ca41cf88edd08453c50995d4f383c0aebb3c240
LucasLVianna <lucaslanavianna.lv@gmail.com>
2026-03-09 11:56:40 -0300

    Creating dashboard directory, home.php (starting navigator bar)

--- dashboard/home.php
+++ dashboard/home.php
@@ -3,14 +3,34 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+    </head>
+
+    <body>
+        <header>
+            <button id="colorMode">White/dark mode</button>
+        </header>
+        <aside class="sideNavBar">
+            <div>
+                <h2>MyKeeper</h2>
+            </div>
+            <div>
+                <nav>
+                    <button id="inicioButtonLink">Início</button>
+                    <button id="inventarioButtonLink"></button>
+                    <button id="avencerButtonLink">A Vencer</button>
+                    <button id="comprasButtonLink">Compras</button>
+                    <button id="receitasButtonLink">Receitas</button>
+                    <button id="historicoButtonLink">Historico</button>
+                </nav>
+            </div>
+        </aside>
+    </body>
+
+    </html>
