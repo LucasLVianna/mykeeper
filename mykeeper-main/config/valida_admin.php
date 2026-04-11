8b6185d859ec3a166ef2038ec1594fb046ebd941
alan.vareschini <alan.lopespv@gmail.com>
2026-04-11 19:08:15 -0300

    preparando para merge

--- mykeeper-main/config/valida_admin.php
+++ mykeeper-main/config/valida_admin.php
@@ -1,6 +1,9 @@
 <?php
+if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
-    if (empty($_SESSION['admin']) || $_SESSION['admin'] !== true) {
-        header("Location: admin_login.php");
-        exit;
-    }
+}
+
+if (empty($_SESSION['admin']) || $_SESSION['admin'] !== true) {
+    header("Location: /mykeeper/src/Views/admin_login.php");
+    exit;
+}
